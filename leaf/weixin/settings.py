c9316d3c5a5bb02adf730f6082ea9ed02e9d9780
桂小方 <guiqiqi187@gmail.com>
2020-01-26 22:56:23 +0800

    微信公众平台通讯设置

---
+++ leaf/weixin/settings.py
@@ -0,0 +1,13 @@
+"""微信公众平台设置"""
+
+Interface = "callback"  # 微信公众平台的回调地址
+
+class User:
+    """用户信息相关设置"""
+    Language = "zh_CN"  # 获取用户信息的语言版本
+
+class Message:
+    """消息相关设置"""
+    EmptyReply = "success"  # 回复空消息
+    TimeOut = 4.1  # 每个回复消息的超时时间 - 推荐小于 5 秒
+    ExclusionLength = 128  # 消息排重队列长度
