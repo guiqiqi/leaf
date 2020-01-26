edbb5e35eafb70922e1a649209ddfce2b1191965
桂小方 <guiqiqi187@gmail.com>
2020-01-26 22:28:57 +0800

    api网关支持

---
+++ leaf/api/__init__.py
@@ -0,0 +1,12 @@
+"""
+Leaf 框架 API 支持:
+    wrap - 一个返回值 API 包装器
+    settings - API 相关设置
+    converter - 类型转换器注册表
+    iplimit - 基于 IP 地址的访问过滤
+"""
+
+from . import settings
+from .wrapper import wrap
+from .wrapper import iplimit
+from .wrapper import converter
