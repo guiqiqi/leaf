e049f08aaa2a04eee649477b0122786ba7b319f7
桂小方 <guiqiqi187@gmail.com>
2020-01-26 22:24:59 +0800

    Leaf核心模块

---
+++ leaf/core/__init__.py
@@ -0,0 +1,27 @@
+"""
+Leaf 框架核心部分:
+    tools - 常用的工具函数库封装
+    algorithm - 常用的数据结构算法封装
+
+    error - 错误与日志记录
+    events - 一个事件管理器
+    wrapper - 常用的装饰器封装
+    database - 提供一个连接池模块+MongoDB连接池
+    schedule - 提供一个任务计划调度模块
+
+    modules - 一个用于保存运行时实例的字典
+"""
+
+from . import tools
+from . import abstract
+from . import algorithm
+
+from . import error
+from . import events
+from . import wrapper
+from . import database
+from . import schedule
+
+
+# modules 保存 Leaf 框架运行时实例
+modules = algorithm.AttrDict()
