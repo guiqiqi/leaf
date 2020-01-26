5780e90d70b19ca39630f4a6c12d355339f93d30
桂小方 <guiqiqi187@gmail.com>
2020-01-26 22:51:04 +0800

    产品数据库模型

---
+++ leaf/selling/commodity/product.py
@@ -0,0 +1,35 @@
+"""产品类型数据库模型"""
+
+import mongoengine
+
+
+class ProductParameters(mongoengine.EmbeddedDocument):
+    """
+    产品参数内嵌文档:
+        name: 选项名称
+        options: 可选项值列表
+    """
+
+    name = mongoengine.StringField()
+    options = mongoengine.ListField()
+
+
+class Product(mongoengine.Document):
+    """
+    产品类数据模型:
+        name: 产品名称
+        describe: 产品描述
+        addtion: 产品额外描述
+        tags: 产品标签列表
+        onsale: 产品是否上架
+        parameters: 产品可选项目列表
+        extension: 扩展数据存储
+    """
+
+    name = mongoengine.StringField()
+    describe = mongoengine.StringField()
+    addition = mongoengine.StringField()
+    tags = mongoengine.ListField(field=mongoengine.StringField())
+    parameters = mongoengine.EmbeddedDocumentListField(ProductParameters)
+    onsale = mongoengine.BooleanField(default=True)
+    extensions = mongoengine.DictField(default=dict)
