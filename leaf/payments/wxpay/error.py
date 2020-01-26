a4cba2d3abc88537136f42e7077b7595749796d7
桂小方 <guiqiqi187@gmail.com>
2020-01-26 22:37:00 +0800

    微信支付错误定义

---
+++ leaf/payments/wxpay/error.py
@@ -0,0 +1,15 @@
+"""微信支付错误类定义"""
+
+from ...core.error import Error
+
+
+class WXPayError(Error):
+    """微信支付错误"""
+    code = 11000
+    description = "微信支付错误父类"
+
+
+class WXPaySignatureError(WXPayError):
+    """微信支付签名错误"""
+    code = 11001
+    description = "签名验证错误 - 警惕端口扫描"
