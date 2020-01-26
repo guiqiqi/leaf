9695336dc9f83ae03e911c9d18fbda5c4ce5b8dc
桂小方 <guiqiqi187@gmail.com>
2020-01-26 22:47:35 +0800

    认证数据库模型

---
+++ leaf/rbac/model/auth.py
@@ -0,0 +1,24 @@
+"""认证记录数据库模型"""
+
+import mongoengine
+
+from . user import User
+
+
+class Authentication(mongoengine.Document):
+    """
+    身份验证内嵌文档模型:
+        user: 被建立登陆方式的用户id
+        index: 用户索引(账户名-主键)
+        salt: 用户密码盐
+        token: 用户密码加盐之后的哈希值
+        status: 验证方式是否可用
+        description: 身份验证方式描述
+    """
+    
+    index = mongoengine.StringField(primary_key=True)
+    user = mongoengine.ObjectIdField(required=True)
+    salt = mongoengine.StringField(required=True)
+    token = mongoengine.StringField(required=True)
+    status = mongoengine.BooleanField(default=True)
+    description = mongoengine.StringField(default=str)
