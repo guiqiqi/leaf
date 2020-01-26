384fe06fdff11c6ef15e2f5181dd26c3994992a3
桂小方 <guiqiqi187@gmail.com>
2020-01-26 22:45:56 +0800

    Token插件常量定义

---
+++ leaf/plugins/accesstoken/const.py
@@ -0,0 +1,17 @@
+"""
+AccessToken 获取插件常量定义
+"""
+
+# 请求中携带的参数键
+GRANT_TYPE = "grant_type"
+APPID = "appid"
+SECRET = "secret"
+TYPE = "client_credential"
+
+# 响应中携带的参数键
+TOKEN = "access_token"
+EXPIRES = "expires_in"
+
+# 错误响应中的键
+CODE = "errcode"
+MESSAGE = "errmsg"
