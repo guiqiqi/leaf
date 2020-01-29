2676170d9b53ea838ebece4252dc781a3c415f3f
桂小方 <guiqiqi187@gmail.com>
2020-01-29 10:28:56 +0800

    JWT Token主要实现

---
+++ leaf/rbac/jwt/token.py
@@ -0,0 +1,54 @@
+"""JWT Token 类"""
+
+from typing import Tuple, Optional, Callable, NoReturn
+
+from ...core.tools import web
+from ...core.tools import time
+from ...core.tools import encrypt
+
+from . import const
+from . import settings
+
+
+class Token:
+    """Token 计算与生成器"""
+
+    def __init__(self, secret: str):
+        """
+        Token 类构造函数:
+            parts: 部分令牌
+            secret: 签名密钥
+            algorithm: 在设置头部时指定算法
+        """
+        self.__parts = list()
+        self.__secret: str = secret
+        self.__algorithm: Callable[[bytes, bytes], str] = None
+
+    def header(self, algorithm: Optional[Tuple[str, Callable]]
+               = settings.Signature.Algorithm) -> NoReturn:
+        """计算头部字符串"""
+        header = const.Header.make(algorithm[0])
+        self.__algorithm = algorithm[1]
+        text = web.JSONcreater(header)
+        self.__parts.append(encrypt.base64encode_url(text.encode()).decode())
+
+    def payload(self, issuer: str, audience: str,
+                period: int = settings.Signature.ValidPeriod,
+                **kwargs) -> NoReturn:
+        """计算载荷部分的值"""
+        now = time.now()
+        payload = {
+            const.Payload.Issuer: issuer,
+            const.Payload.Audience: audience,
+            const.Payload.IssuedAt: now,
+            const.Payload.Expiration: now + period
+        }
+        payload.update(kwargs)
+        text = web.JSONcreater(payload)
+        self.__parts.append(encrypt.base64encode_url(text.encode()).decode())
+
+    def issue(self) -> str:
+        """签名之后返回可用的Token"""
+        content = '.'.join(self.__parts)
+        signature = self.__algorithm(content.encode(), self.__secret.encode())
+        return content + '.' + encrypt.base64encode_url(signature).decode()
