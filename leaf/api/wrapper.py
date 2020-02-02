3e93ff1f2e64f3d57dac985de0f8c26e9f760a9a
桂小方 <guiqiqi187@gmail.com>
2020-02-02 10:21:23 +0800

    增加盐查询缓存

--- leaf/api/wrapper.py
+++ leaf/api/wrapper.py
@@ -165,11 +165,11 @@ def require(pointname: str) -> Callable:
             # 通过 payload 获取用户盐并判断 Token 是否正确
             try:
                 userid = payload.get(rbac.jwt.const.Payload.Issuer)
-                auth = rbac.functions.auth.Retrieve.byindex(str(userid))
-                assert not auth is None
-                verification.signature(auth.salt)
+                salt = rbac.functions.auth.Retrieve.saltbyindex(str(userid))
+                # assert not auth is None
+                verification.signature(salt)
             except AssertionError as _error:
-                return settings.Authorization.UnAuthorized(auth)
+                return settings.Authorization.UnAuthorized(_error)
             except error.Error as _error:
                 logger.warning(_error)
                 return settings.Authorization.UnAuthorized(_error)
