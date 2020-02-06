bb5fb7a96de628f7925a5229bf3ace9c6b30fc43
桂小方 <guiqiqi187@gmail.com>
2020-02-06 19:19:14 +0800

    增加用户/用户组文档操作函数

---
+++ leaf/rbac/functions/group.py
@@ -0,0 +1,33 @@
+"""用户组操作的相关函数集合"""
+
+from typing import NoReturn, List
+
+from bson import ObjectId
+
+from . import error
+from ..model.group import Group
+
+
+class Create:
+    """创建用户组的静态函数集合"""
+
+
+class Retrieve:
+    """查询用户组的静态函数集合"""
+
+    @staticmethod
+    def byid(groupid: ObjectId) -> Group:
+        """通过用户组 ID 删除用户组记录"""
+        # pylint: disable=no-member
+        found: List[Group] = Group.objets(id=groupid)
+        if not found:
+            raise error.GroupNotFound(str(groupid))
+        return found.pop()
+
+
+class Update:
+    """更新/编辑用户组的静态函数集合"""
+
+
+class Delete:
+    """删除用户组的静态函数结合"""
---
+++ leaf/rbac/functions/user.py
@@ -0,0 +1,99 @@
+"""用户相关的数据库操作函数集合"""
+
+from typing import List
+
+import mongoengine
+from bson import ObjectId
+
+from . import error
+from .. import settings
+from ..model import User
+from ..model import UserIndex
+
+
+class Create:
+    """创建用户静态函数集合"""
+
+
+class Retrieve:
+    """查询用户静态函数集合"""
+
+    @staticmethod
+    def byindex(typeid: str, value: str) -> List[User]:
+        """通过 Index 用户索引文档查询用户记录"""
+        # pylint: disable=no-member
+        return User.objects(indexs__value=value,
+                            indexs__typeid=typeid)
+
+    @staticmethod
+    def byid(userid: ObjectId) -> User:
+        """通过用户 Id 查询用户文档"""
+        # pylint: disable=no-member
+        users = User.objects(id=userid)
+        if not users:
+            raise error.UserNotFound(str(userid))
+        return users.pop()
+
+    @staticmethod
+    def initialized(userid: ObjectId) -> bool:
+        """查询用户是否被初始化"""
+        user: User = Retrieve.byid(userid)
+        if not user.indexs:
+            return True
+        return False
+
+
+class Update:
+    """更新用户静态函数集合"""
+
+    @staticmethod
+    def index(userid: ObjectId, userindex: UserIndex) -> User:
+        """
+        该函数将为用户新增一种索引类型:
+            1. 首先会检查指定的索引值是否被其他用户绑定过
+            2. 尝试插入检查是否发生 unique 错误
+        """
+        # 检查是否其他用户已经绑定过
+        typeid, value = userindex.typeid, userindex.value
+        if Retrieve.byindex(typeid, value):
+            raise error.UserIndexValueBound(typeid + " - " + value)
+
+        try:
+            user: User = Retrieve.byid(userid)
+            user.indexs.append(userindex)
+        except mongoengine.NotUniqueError as _error:
+            raise error.UserIndexTypeBound(typeid)
+        else:
+            return user.save()
+
+    @staticmethod
+    def inituser(userid: ObjectId) -> User:
+        """
+        该函数将为用户创建ID索引
+        创建一个用户的接口调用顺序如下:
+            1. 首先调用创建用户接口 - View 层处理
+            2. 调用为用户设置密码接口 - auth.Create.withuserid
+            3. 为用户设置文档ID索引 - 该接口
+        """
+        try:
+            # pylint: disable=no-member
+            user: List[User] = User.objects(id=userid)
+            user: User = user.pop()
+
+            # 为用户创建Id索引
+            typeid, description = settings.User.Index.Id
+            idindex = UserIndex(
+                typeid=typeid, value=str(userid),
+                description=description)
+            user.indexs.append(idindex)
+
+        except mongoengine.NotUniqueError as _error:
+            raise error.UserInitialized(str(userid))
+        except IndexError as _error:
+            raise error.UserNotFound(str(userid))
+        else:
+            return user.save()
+
+
+class Delete:
+    """删除用户静态函数集合"""
