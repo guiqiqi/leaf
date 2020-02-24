a374998f247be1a8b2bb25b59606a724cf813c79
Doge-GUI <guiqiqi187@gmail.com>
2020-02-25 00:07:16 +0800

    更新配置/启动示例文件

--- config.py
+++ config.py
@@ -1,6 +1,10 @@
 """Leaf 框架配置示例文件"""
 
+# 以下是系统常量, 请勿进行修改
 
+###############################################################
+
+# 静态字典类
 class Static(dict):
     """静态 Dict 类"""
 
@@ -21,65 +25,110 @@ class Static(dict):
 CRITICAL, FATAL, ERROR = 50, 50, 40
 WARNING, INFO, DEBUG, NOTSET = 30, 20, 10, 0
 
-# 配置文件开始
+###############################################################
+
+# 配置文件开始, 您可以开始您的配置
+
+# 服务相关配置
+# domain - 您部署 Leaf 的域名
+server = Static({
+    "domain": "xx.xxx.com"
+})
+
 
 # 错误与日志处理配置
 logging = Static({
-    "level": INFO,
-    "rcfile": "leaf.log",
-    "format": None,
 
-    # 控制台输出配置
+    # 默认的日志等级; 一般不需要修改
+    "level": INFO,
+
+    # 日志的存储位置及名称
+    # 请确保程序有该处的读写权限
+    "rcfile": "leaf.log",
+
+    # 日志格式 - None 表示使用默认; 更多配置请参考
+    # https://docs.python.org/3/library/logging.html
+    "format": None,  
+
+    # 控制台日志输出配置
+    # level - 单独设置控制台的输出等级
+    # format - 单独设置控制台的输出格式; None 表示继承上面的格式
     "console": Static({
         "level": DEBUG,
         "format": None  # 表示使用父级 logger 配置
     }),
 
-    # 文件输出配置
+    # 文件日志输出配置
+    # level - 单独设置文件的输出等级
+    # format - 单独设置文件的输出格式; None 表示继承上面的格式
     "file": Static({
         "level": ERROR,
         "format": None  # 表示使用父级 logger 配置
     })
 })
 
+
 # 微信公众平台配置
+# appid - 微信公众平台 AppId
+# aeskey - 微信公众平台 AESKey
+# token - 微信公众平台消息传递 Token
 weixin = Static({
     "appid": "wxabcd1234abcd1234",
     "aeskey": "s5d6t7vybotcre3465d68f7ybvtd4sd5687g8huhyvt",
     "token": "s547d6figobunb67568d8f7g8ohjiks1"
 })
 
+
 # 微信支付配置
+# appid - 微信支付关联的公众号 AppId
+# mchid - 微信支付商户 Id
+# apikey - 微信支付 API 密钥
 wxpay = Static({
     "appid": "wxabcd1234abcd1234",
     "mchid": "8888888888",
     "apikey": "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
 
     # 回调地址
+    # pay - 表示支付成功的回调地址
+    # refund - 表示退款成功的回调地址
+    # 除非您有其余服务部署在以下地址, 否则请不要修改
     "callbacks": {
-        "pay": "https://xxx.com/wxpay/notify",
-        "refund": "https://xxx.com/wxpay/notify_refund"
+        "pay": "https://" + server.domain +"/wxpay/notify",
+        "refund": "https://" + server.domain + "/wxpay/notify_refund"
     },
 
-    # 证书位置
+    # 退款操作证书位置
+    # 第一个为签名文件路径, 第二个为证书文件路径
+    # 请填写绝对路径
+    # 如果您确定自己的不需要使用退款功能, 可以随意填写
     "cert": (
         "root\\key.pem",
         "root\\cert.pem"
     )
 })
 
+
 # 数据库配置
+# database - 需要连接的数据库
+# host - 数据库服务器地址
+# port - 数据库服务端口
+# username - 用户名; None 表示不需要
+# password - 密码; None 表示不需要
+# timeout - 连接超时时间; 以秒为单位
 database = Static({
-    "database": "test", # 需要连接的数据库
-    "host": "localhost",  # 数据库服务器地址
-    "port": 27017,  # 数据库服务端口
-    "username": None,  # 数据库用户名 - None 表示不需要
-    "password": None,  # 数据库密码 - None 表示不需要
-    "timeout": 5  # 连接超时时间 - 秒为单位
+    "database": "test",
+    "host": "localhost",
+    "port": 27017,
+    "username": None,
+    "password": None,
+    "timeout": 5
 })
 
+
 # 插件配置
+# autorun - Leaf 启动时插件是否自动运行
+# directory - 插件加载位置; None 表示不需要
 plugins = Static({
-    "autorun": False,  # 插件是否自动运行
-    "directory": None  # 是否从别处加载插件 - None 表示不需要
+    "autorun": False,
+    "directory": None
 })
--- run.py
+++ run.py
@@ -6,23 +6,28 @@ import flask
 import leaf
 import config
 
-print("Initialing leaf modules...")
 init = leaf.Init()
 init.kernel()
 init.logging(config.logging)
 init.server()
 init.database(config.database)
-init.plugins(config.plugins)
-init.weixin(config.weixin)
-init.wxpay(config.wxpay)
+
+# 以下模块请根据需要启用/禁用初始化
+init.plugins(config.plugins)  # 插件模块
+init.weixin(config.weixin)  # 微信公众平台支持模块
+init.wxpay(config.wxpay)  # 微信支付支持模块
 
 # 获取服务模块
 server: flask.Flask = leaf.modules.server
-plugins: leaf.plugins.Manager = leaf.modules.plugins
-logger: logging.Logger = leaf.modules.logging.logger
-events: leaf.core.events.Manager = leaf.modules.events
-wxpay: leaf.core.algorithm.AttrDict = leaf.payments.wxpay
-weixin: leaf.core.algorithm.AttrDict = leaf.modules.weixin
-schedules: leaf.core.schedule.Manager = leaf.modules.schedules
 
-print("All modules has been imported...")
+# 以下模块的获取根据您上面的启用情况以及需求进行
+# plugins: leaf.plugins.Manager = leaf.modules.plugins  # 插件系统模块实例
+# logger: logging.Logger = leaf.modules.logging.logger  # 日志系统模块实例
+# events: leaf.core.events.Manager = leaf.modules.events  # 事件系统模块实例
+# wxpay: leaf.core.algorithm.AttrDict = leaf.payments.wxpay  # 微信支付模块实例
+# weixin: leaf.core.algorithm.AttrDict = leaf.modules.weixin  # 微信公众平台模块实例
+# schedules: leaf.core.schedule.Manager = leaf.modules.schedules  # 任务调度模块实例
+
+# 如果需要启用 Flask 自带的 Web Server 进行调试
+# 请取消下面一行的注释并给定 Flask 运行参数即可
+# server.run(host="0.0.0.0", port=80, ...)
