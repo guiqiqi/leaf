fdef42bdf2f2582951a374618695c327cca5be7a
Doge-GUI <guiqiqi187@gmail.com>
2020-02-27 11:26:17 +0800

    完成产品相关接口
    新增商品操作接口文件

--- leaf/views/commodity/product.py
+++ leaf/views/commodity/product.py
@@ -8,9 +8,9 @@ from ...api import wrapper
 from ...api import validator
 from ...core.tools import web
 
-# from ...selling.commodity import Stock
+from ...selling.commodity import Stock
 from ...selling.commodity import Product
-# from ...selling.commodity import StocksGenerator
+from ...selling.commodity import StocksGenerator
 from ...selling.commodity import ProductParameter
 from ...selling.functions import product as functions
 
@@ -101,7 +101,7 @@ def add_parameter_for_product(productid: str) -> List[ProductParameter]:
 @wrapper.wrap("product")
 def update_product_status(productid: str) -> Product:
     """更新产品在售状态"""
-    onsale: bool = request.form.get("onsale", default=True, type=bool)
+    onsale: bool = bool(request.form.get("onsale", default=1, type=int))
     return functions.Update.onsale(productid, onsale)
 
 
@@ -119,3 +119,71 @@ def create_product() -> Product:
     product = Product(name=name, addition=addition,
                       description=description, tags=tags)
     return product.save()
+
+
+@commodity.route("/products/<string:productid>", methods=["PUT"])
+@wrapper.require("leaf.views.commodity.product.update")
+@wrapper.wrap("product")
+def update_product(productid: str) -> Product:
+    """更新产品信息"""
+    productid = validator.objectid(productid)
+    name: str = request.form.get("name", default='', type=str)
+    addition: str = request.form.get("addition", default='', type=str)
+    description: str = request.form.get("description", default='', type=str)
+    tags: str = request.form.get("tags", default="[]", type=str)
+    tags: List[str] = web.JSONparser(tags)
+    tags = [tag.strip().lower() for tag in tags]
+    product = functions.Retrieve.byid(productid)
+    product.name = name
+    product.addition = addition
+    product.description = description
+    product.tags = tags
+    return product.save()
+
+
+@commodity.route("/products/<string:productid>/goods", methods=["GET"])
+@wrapper.require("leaf.views.commodity.product.get")
+@wrapper.wrap("goods")
+def get_goods_from_product(productid: str) -> List[Stock]:
+    """获取当前产品已经生成的所有商品列表"""
+    productid = validator.objectid(productid)
+    product = functions.Retrieve.byid(productid)
+    # pylint: disable=no-member
+    return Stock.objects(product=product)
+
+
+@commodity.route("/products/<string:productid>/goods", methods=["POST"])
+@wrapper.require("leaf.views.commodity.product.update")
+@wrapper.wrap("goods")
+def generate_goods_from_product(productid: str) -> List[Stock]:
+    """生成商品列表"""
+    save: bool = bool(request.form.get("save", default=0, type=int))
+    price: float = request.form.get("price", default=0.0, type=float)
+    inventory: int = request.form.get("inventory", default=0, type=int)
+    currency: str = request.form.get("currency", default="CNY", type=str)
+    productid = validator.objectid(productid)
+    product = functions.Retrieve.byid(productid)
+    generator = StocksGenerator(product)
+    generator.setall(price, currency, inventory)
+    generator.calculate()
+
+    # 判断是否保存
+    if save:
+        generator.clean()
+        generator.save()
+        # pylint: disable=no-member
+        return Stock.objects(product=product)
+
+    return generator.goods
+
+
+@commodity.route("/products/<string:productid>/goods", methods=["DELETE"])
+@wrapper.require("leaf.views.commodity.product.update")
+@wrapper.wrap("status")
+def clean_all_goods_from_product(productid: str) -> bool:
+    """清除所有的生成商品"""
+    productid = validator.objectid(productid)
+    product = functions.Retrieve.byid(productid)
+    generator = StocksGenerator(product)
+    generator.clean()
+    return True
