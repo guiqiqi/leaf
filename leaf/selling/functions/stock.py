9fe4233507e84163d2451c32845a05f32326eb18
Doge-GUI <guiqiqi187@gmail.com>
2020-04-29 08:35:24 +0800

    完成了商品模块的CRUD开发

--- leaf/selling/functions/stock.py
+++ leaf/selling/functions/stock.py
@@ -18,6 +18,30 @@ class Retrieve:
             raise error.StockNotFound(goodid)
         return goods[0]
 
+    @staticmethod
+    def byname(name: str, individual: bool = False) -> List[Stock]:
+        """根据名称查找商品"""
+        # pylint: disable=no-member
+        if individual:
+            return Stock.objects(name__icontains=name, individual=True)
+        return Stock.objects(name__icontains=name)
+
+    @staticmethod
+    def bytags(tags: List[str], individual: bool = False) -> List[Stock]:
+        """根据标签查找商品"""
+        # 首先对标签进行头尾空格处理, 小写化处理
+        tags = [tag.strip().lower() for tag in tags]
+
+        #pylint: disable=no-member
+        if individual:
+            queryset: List[Stock] = Stock.objects(tags=tags[0], individual=True)
+        else:
+            queryset: List[Stock] = Stock.objects(tags=tags[0])
+
+        for tag in tags[1::]:
+            queryset = queryset.filter(tags=tag)
+        return queryset
+
 
 class Delete:
     """删除相关函数"""
@@ -29,3 +53,10 @@ class Create:
 
 class Update:
     """更新相关函数"""
+
+    @staticmethod
+    def onsale(goodid: str, status: bool) -> Stock:
+        """更新商品在售信息"""
+        good = Retrieve.byid(goodid)
+        good.onsale = status
+        return good.save()
--- leaf/views/commodity/stock.py
+++ leaf/views/commodity/stock.py
@@ -4,9 +4,12 @@ from typing import List
 from flask import request
 
 from . import commodity
+from ...core.tools import web
 from ...api import wrapper
 from ...api import validator
-# from ...core.tools import web
+from ...selling import error
+from ...selling import settings
+from ...selling.functions import stock as functions
 
 from ...selling.commodity import Stock
 
@@ -15,9 +18,9 @@ from ...selling.commodity import Stock
 @wrapper.require("leaf.views.commodity.product.get")
 @wrapper.wrap("goods")
 def query_individual_goods() -> List[Stock]:
-    """查询所有的商品"""
-
-    individual: bool = bool(request.args.get("individual", default=1, type=int))
+    """查询所有的独立商品"""
+    individual: bool = bool(request.args.get(
+        "individual", default=1, type=int))
     count: int = request.args.get("count", default=0, type=int)
     previous: str = request.args.get("previous", default='0' * 24, type=str)
     previous = validator.objectid(previous)
@@ -28,7 +31,113 @@ def query_individual_goods() -> List[Stock]:
     return list(Stock.objects(id__gt=previous)).limit(count)
 
 
-# @commodity.route("/goods", methods=["PUT"])
-# @commodity.route("/goods", methods=["POST"])
-# @commodity.route("/goods/<string: goodid>", methods=["GET"])
-# @commodity.route("/goods/<string: goodid>", methods=["DELETE"])
+@commodity.route("/goods/tags/<string:tags>", methods=["GET"])
+@wrapper.require("leaf.views.commodity.product.get")
+@wrapper.wrap("goods")
+def query_individual_goods_bytags(tags: str) -> List[Stock]:
+    """根据标签查询所有的独立商品 - 以 ',' 分割"""
+    tags: List[str] = tags.split(',')
+    return functions.Retrieve.bytags(tags, individual=True)
+
+
+@commodity.route("/goods/name/<string:name>", methods=["GET"])
+@wrapper.require("leaf.views.commodity.product.get")
+@wrapper.wrap("goods")
+def query_individual_goods_byname(name: str) -> List[Stock]:
+    """根据名称查询所有的独立商品"""
+    return functions.Retrieve.byname(name, individual=True)
+
+
+@commodity.route("/goods/<string:goodid>", methods=["PUT"])
+@wrapper.require("leaf.views.commodity.product.update")
+@wrapper.wrap("good")
+def update_specific_good(goodid: str) -> Stock:
+    """更新一个特定的产品信息"""
+    goodid = validator.objectid(goodid)
+    good = functions.Retrieve.byid(goodid)
+    good.name: str = request.form.get("name", default='', type=str)
+    good.description: str = request.form.get(
+        "description", default='', type=str)
+    good.addition: str = request.form.get("addition", default='', type=str)
+    good.price: float = request.form.get("price", default=0.00, type=float)
+    currency: str = request.form.get(
+        "currency", default=settings.General.DefaultCurrency, type=str)
+
+    # 检查货币类型是否允许
+    if not currency in settings.General.AllowCurrency:
+        raise error.InvalidCurrency(currency)
+
+    good.currency = currency
+    good.inventory: int = request.form.get("inventory", default=0, type=int)
+    return good.save()
+
+
+@commodity.route("/goods", methods=["POST"])
+@wrapper.require("leaf.views.commodity.product.create")
+@wrapper.wrap("good")
+def create_good_individually() -> Stock:
+    """
+    创建一个独立商品
+    这里不设置 tags, extensions - 留作单独 API 接口设置
+    """
+    name: str = request.form.get("name", default='', type=str)
+    product = validator.objectid('0' * 24)
+    description: str = request.form.get("description", default='', type=str)
+    addition: str = request.form.get("addition", default='', type=str)
+    price: float = request.form.get("price", default=0.00, type=float)
+    currency: str = request.form.get(
+        "currency", default=settings.General.DefaultCurrency, type=str)
+
+    # 检查货币类型是否允许
+    if not currency in settings.General.AllowCurrency:
+        raise error.InvalidCurrency(currency)
+
+    inventory: int = request.form.get("inventory", default=0, type=int)
+    stock = Stock(individual=True, product=product, name=name,
+                  attributes=dict(), description=description,
+                  addition=addition, tags=list(), price=price,
+                  currency=currency, inventory=inventory, onsale=True)
+    return stock.save()
+
+
+@commodity.route("/goods/<string:goodid>/tags", methods=["PUT"])
+@wrapper.require("leaf.views.commodity.product.update")
+@wrapper.wrap("good")
+def update_good_tags(goodid: str) -> Stock:
+    """更改商品标签"""
+    goodid = validator.objectid(goodid)
+    good = functions.Retrieve.byid(goodid)
+    tags: str = request.form.get("tags", default="[]", type=str)
+    tags: List[str] = web.JSONparser(tags)
+    good.tags = [tag.strip().lower() for tag in tags]
+    return good.save()
+
+
+@commodity.route("/goods/<string:goodid>/onsale", methods=["PUT"])
+@wrapper.require("leaf.views.commodity.product.update")
+@wrapper.wrap("good")
+def update_good_status(goodid: str) -> Stock:
+    """更新商品在售状态"""
+    goodid = validator.objectid(goodid)
+    onsale: bool = bool(request.form.get("onsale", default=1, type=int()))
+    return functions.Update.onsale(goodid, onsale)
+
+
+@commodity.route("/goods/<string:goodid>", methods=["GET"])
+@wrapper.require("leaf.views.commodity.product.get")
+@wrapper.wrap("good")
+def query_specific_good(goodid: str) -> Stock:
+    """查询一个指定的商品信息"""
+    goodid = validator.objectid(goodid)
+    return functions.Retrieve.byid(goodid)
+
+
+@commodity.route("/goods/<string:goodid>", methods=["DELETE"])
+@wrapper.require("leaf.views.commodity.product.delete")
+@wrapper.wrap("result")
+def delete_one_good(goodid: str) -> bool:
+    """删除一个指定的商品"""
+    goodid = validator.objectid(goodid)
+    good = functions.Retrieve.byid(goodid)
+    good.delete()
+    return True
