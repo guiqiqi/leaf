b3d23972fde3a6ce34d297b515704dd29b0b2a2f
Doge-GUI <guiqiqi187@gmail.com>
2020-04-29 08:35:00 +0800

    新增交易货币类型设置
    增加了相关的设置项目与错误说明

--- leaf/selling/settings.py
+++ leaf/selling/settings.py
@@ -2,6 +2,16 @@
 
 from ..core.schedule import HOUR
 
+
+class General:
+    """与交易相关的全局设置"""
+
+    DefaultCurrency = "CNY"  # 默认的货币类型
+    AllowCurrency = {
+        "CNY", "USD", "RUB"
+    }  # 允许设置的价格单位
+
+
 class Product:
     """产品相关的设置"""
 
