70f0fddf3f50b9ce63e95018dd24eb6b99cc4039
Doge-GUI <guiqiqi187@gmail.com>
2020-05-24 10:33:24 +0800

    更新发布文件

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 
 import setuptools
 
-VERSION = "1.0.9.1.dev1"
+VERSION = "1.0.9.1.dev2"
 
 # 读取项目说明
 with open("README.md", "r", encoding="utf-8") as handler:
@@ -27,6 +27,9 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/guiqiqi/leaf",
     packages=setuptools.find_packages(),
+    package_data={
+        "leaf.weixin": ["errcodes.json"]
+    },
     classifiers=[
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
